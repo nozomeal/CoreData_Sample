8b9d23991dca1194dd51474d77601ab77d73ad58
坂本のぞみ <g15938sn@gm.tsuda.ac.jp>
2017-08-11 14:16:28 +0900

    削除機能追加

---
+++ CoreData_Sample/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  CoreData_Sample
+//
+//  Created by 坂本のぞみ on 2017/08/11.
+//  Copyright © 2017年 g15938sn. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
